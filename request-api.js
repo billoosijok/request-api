2c41ff220ba2f5a2825a5e4f52309669bb190d1b
Belal Sejouk <billoosijok@gmail.com>
2017-04-18 19:39:17 -0700

    added ability to require dependencies

--- request-api.js
+++ request-api.js
@@ -1,3 +1,4 @@
+require('https://code.jquery.com/jquery-3.2.1.min.js', 'js');
 
 function apiRequest(params) {
 	
@@ -17,4 +18,40 @@ function apiRequest(params) {
 		  return false;
 		});
 	});
+}
+
+function require(fileurl, type) {
+	
+	var element = "";
+	var attributes = {}; 
+
+	switch (type.toLowerCase()) {
+		case 'css':
+			element = 'link';
+			attributes.rel 	= "stylesheet" 
+			attributes.type	= "text/css" 
+			attributes.href = fileurl
+
+			break;
+
+		case 'js':
+		case 'javascript':
+			element = 'script';
+			attributes.type = "text/javascript" 
+			attributes.src  = fileurl
+			break;
+
+		default:
+			return false;
+	}
+
+	var fileElement = document.createElement(element);
+
+	for (attr in attributes) {
+		fileElement.setAttribute(attr, attributes[attr]);
+	}
+
+	document.head.appendChild(fileElement);
+
+	return true
 }
